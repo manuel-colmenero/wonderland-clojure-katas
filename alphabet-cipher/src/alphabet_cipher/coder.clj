e0164e7402e2bf71eeb416933447d9da0760f428
Manuel Colmenero <manuel@colmenero.es>
2020-05-03 15:28:16 +0200

    Solve the alphabet cipher problem

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,38 @@
 (ns alphabet-cipher.coder)
 
+(def ^:private letters (vec "abcdefghijklmnopqrstuvwxyz"))
+
+(defn- get-row [l]
+  (sequence
+    (comp (drop (.indexOf letters l)) (take (count letters)))
+    (cycle letters)))
+
+(defn- encode-letter [[k l]]
+  (nth (get-row l) (.indexOf letters k)))
+
+(defn- decode-letter [[k l]]
+  (nth letters (.indexOf (get-row k) l)))
+
+(defn- decipher-letter [[c l]]
+  (nth letters (.indexOf (get-row l) c)))
+
+(defn- transform [mapper keyword message]
+  (transduce
+    (comp (partition-all 2) (map mapper)) str
+    (interleave keyword message)))
+
+(defn- uncycle [string]
+  (some (fn [len]
+          (let [chunk (take len string)]
+            (if (every? #(= % (take (count %) chunk)) (partition-all len string))
+              (reduce str chunk))))
+        (range 1 (count string))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (transform encode-letter (cycle keyword) message))
 
 (defn decode [keyword message]
-  "decodeme")
+  (transform decode-letter (cycle keyword) message))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (uncycle (transform decipher-letter cipher message)))
