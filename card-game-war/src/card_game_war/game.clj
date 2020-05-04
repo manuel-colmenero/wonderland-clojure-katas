97c26c88ef5b649f3095ebca92b8da09d75b9ffb
Manuel Colmenero <manuel@colmenero.es>
2020-05-04 14:19:03 +0200

    Add solution for the war card game kata

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,39 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn lose-round [cards]
+  (subvec cards 1))
 
-(defn play-game [player1-cards player2-cards])
+(defn win-round [cards opponent stack]
+  (as-> cards $
+      (subvec $ 1)
+      (apply conj $ stack)
+      (conj $ (first opponent) (first cards))))
+
+(defn draw-round [cards]
+  (vec (drop 4 cards)))
+
+(defn draw-stack [p1 p2 stack]
+  (vec (concat (take 4 p1) (take 4 p2) stack)))
+
+(defn play-round [[_ r1] [_ r2]]
+  (compare (.indexOf ranks r1) (.indexOf ranks r2)))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [p1 player1-cards
+         p2 player2-cards
+         stack []]
+    (if (some empty? [p1 p2])
+      [p1 p2]
+      (case (play-round (first p1) (first p2))
+        -1 (recur (lose-round p1) (win-round p2 p1 stack) [])
+        1 (recur (win-round p1 p2 stack) (lose-round p2) [])
+        0 (recur (draw-round p1) (draw-round p2) (draw-stack p1 p2 stack))))))
+
+(defn shuffle-deck []
+  (sequence (comp (partition-all (/ (count cards) 2)) (map vec))
+            (shuffle cards)))
+
+(defn -main []
+  (let [[p1 p2] (shuffle-deck)]
+    (play-game p1 p2)))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,15 +2,49 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
-
 ;; fill in  tests for your game
 (deftest test-play-round
+  (testing "equal values result in a draw"
+    (is (= 0 (play-round [:heart 5] [:diamond 5]))))
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings"))
+    (is (= -1 (play-round [:spade 3] [:club 8])))
+    (is (= 1 (play-round [:heart 7] [:spade 2]))))
+  (testing "queens are higher rank than jacks"
+    (is (= -1 (play-round [:heart :jack] [:club :queen])))
+    (is (= 1 (play-round [:heart :queen] [:club :jack]))))
+  (testing "kings are higher rank than queens"
+    (is (= -1 (play-round [:spade :queen] [:club :king])))
+    (is (= 1 (play-round [:spade :king] [:club :queen]))))
+  (testing "aces are higher rank than kings")
+    (is (= -1 (play-round [:club :king] [:club :ace]))
+    (is (= 1 (play-round [:club :ace] [:club :king])))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "the player loses when they run out of cards"
+    (is (= [[] [[:club :queen][:club :ace]]]
+           (play-game [[:club :queen]] [[:club :ace]])))
+    (is (= [[[:club :queen][:club :ace]] []]
+           (play-game [[:club :ace]] [[:club :queen]]))))
+  (testing "a draw means war"
+    (let [p1 [[:club 4]
+              [:club 1]
+              [:club 2]
+              [:club 3]
+              [:club :ace]]
+          p2 [[:spade 4]
+              [:spade 1]
+              [:spade 2]
+              [:spade 3]
+              [:spade :jack]]]
+      (is (= [[[:club 4]
+               [:club 1]
+               [:club 2]
+               [:club 3]
+               [:spade 4]
+               [:spade 1]
+               [:spade 2]
+               [:spade 3]
+               [:spade :jack]
+               [:club :ace]]
+              []]
+             (play-game p1 p2))))))
