92e2afb01241cd83d9868a155ce6d00a08bffa39
Manuel Colmenero <manuel@colmenero.es>
2020-05-16 01:36:31 +0200

    Add solution for "Fox, Goose, and a Bag of Corn"

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,34 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+(def start-pos [[:fox :goose :corn :you] [:boat] []])
+
+(defn extract [& things]
+  (fn [container]
+    (into [] (remove (into #{:you} things)) container)))
+
+(defn add [& things]
+  (fn [container]
+    (into container (cons :you things))))
+
+(defn transform [stage fns]
+  (into [] (map #(%1 %2) fns stage)))
+
+(defn bring [thing]
+  [[(extract thing) (add thing) identity]
+   [identity (extract thing) (add thing)]])
+
+(defn go-back [& things]
+  [[identity (apply add things) (apply extract things)]
+   [(apply add things) (apply extract things) identity]])
 
 (defn river-crossing-plan []
-  start-pos)
+  (reductions transform
+              start-pos
+              (concat
+                (bring :goose)
+                (go-back)
+                (bring :fox)
+                (go-back :goose)
+                (bring :corn)
+                (go-back)
+                (bring :goose))))
