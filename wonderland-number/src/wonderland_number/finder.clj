7e8764791cf834aa1b4b57dbfb3bb4910890bad3
Manuel Colmenero <manuel@colmenero.es>
2020-06-13 00:26:44 +0200

    Add solution for the wonderland number problem

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,7 @@
 (ns wonderland-number.finder)
 
+(defn is-wonderland-number? [n]
+  (apply = (map #(-> % (* n) str set) (range 2 7))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (->> (range 100000 1000000) (filter is-wonderland-number?) first))
