48a9c88fc93e69b1203ec5e077e384361a4be0ce
Manuel Colmenero <manuel@colmenero.es>
2020-07-01 13:45:50 +0200

    Add solution for the magic square puzzle

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -2,7 +2,24 @@
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
-(defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]])
+(defn is-magic? [v]
+  (let [rows (partition 3 v)
+        cols (apply map list rows)
+        diagonals (map #(map v %) [[0 4 8] [2 4 6]])
+        sums (map #(reduce + %) (concat rows cols diagonals))]
+    (apply = sums)))
+
+(defn -magic-square
+  ([values] (-magic-square [] values))
+  ([head tail]
+   (if (empty? tail)
+     (when (is-magic? head)
+       (into [] (comp (partition-all 3) (map vec)) head))
+     (first
+       (sequence
+         (comp
+           (map #(-magic-square (conj head %) (remove (partial = %) tail)))
+           (filter some?))    
+         tail)))))
+
+(def magic-square (memoize -magic-square))
